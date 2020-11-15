7b10554bbd30feac84b1f624c38fa808f66b7675
maeek <maciek299.ms@gmail.com>
2020-11-15 22:31:25 +0100

    Updated index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,22 @@
+import 'dotenv';
 import express from 'express';
+import path from 'path';
+import ConfigParser from './parser/config-parser';
+import FileHandler from './util/file';
+import logger from './util/log'
 
 const App = express();
 
 App.listen(8080, () => {
-  console.log('Server listening on http://localhost:8080/')
+  logger.log('Server listening on http://localhost:8080/')
+});
+
+
+/**
+ * Testing purposes
+ */
+const file = new FileHandler(path.join(__dirname, '../../test_config_cleaned'))
+file.load((content) => {
+  const conf = new ConfigParser(content);
+  conf.parse()
 });
