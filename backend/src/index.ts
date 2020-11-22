a8014f66c10f51dc01f9c1a5efadc07cdf1f8b91
maeek <maciek299.ms@gmail.com>
2020-11-22 19:04:40 +0100

    Updated index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,6 @@
 import './load-vars';
-// import path from 'path';
-// import FileHandler from './util/file';
 import logger from './util/log'
 import App from './app';
-// import ConfigParser from './haproxy/cfg-parser';
 
 const PORT = process.env.API_PORT || 8080;
 
@@ -11,13 +8,3 @@ App.listen(PORT, () => {
   logger.log(`Server listening on http://localhost:${PORT}/`);
 });
 
-/**
- * Testing purposes
- */
-// const file = new FileHandler(path.join(__dirname, '../../test_config'))
-// file.load((content) => {
-//   const conf = new ConfigParser(content);
-//   conf.parse()
-//   conf.stringify()
-//   // conf.toString()
-// });
